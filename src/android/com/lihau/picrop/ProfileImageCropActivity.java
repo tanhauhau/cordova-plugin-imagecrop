037eb4a34a466e4ed76ad7fd3cc929cecfc12699
Tan Li Hau <lhtan93@gmail.com>
2016-09-13 09:11:02 +0800

    0.0.5

--- src/android/com/lihau/picrop/ProfileImageCropActivity.java
+++ src/android/com/lihau/picrop/ProfileImageCropActivity.java
@@ -15,6 +15,7 @@ import android.os.Bundle;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
